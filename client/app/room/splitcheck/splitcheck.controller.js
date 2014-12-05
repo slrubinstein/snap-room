e7deec1eaf2f9fe3a86b025993176ebd881998c7
Steve Rubinstein <slrubinstein@gmail.com>
2014-12-05 11:49:27 -0500

    improve layout of bill, add some billing logic

--- client/app/room/splitcheck/splitcheck.controller.js
+++ client/app/room/splitcheck/splitcheck.controller.js
@@ -12,20 +12,36 @@ angular.module('roomApp')
 
   	this.billItems = [{user: 'Steve', item: 'sandwich', price: 4, itemTax: 1}];
 
+  	this.subtotal = '';
+
   	this.submit = function() {
   		var userBill = {
   			user: ctrl.user,
   			item: ctrl.item,
-  			price: ctrl.price,
+  			price: Number(ctrl.price),
   			itemTax: ctrl.price * ctrl.tax/100
   		}
 
-  		this.billItems.push(userBill);
+  		ctrl.billItems.push(userBill);
+  		ctrl.runningTotal = ctrl.calculateRunningTotal(ctrl.billItems);
   		ctrl.user = '';
   		ctrl.item = '';
   		ctrl.price = '';
   	}
 
+  	this.calculateRunningTotal = function(billItems) {
+  		var runningTotal = 0;
+  		ctrl.billItems.forEach(function(item) {
+	  		runningTotal += item.price;
+	  		runningTotal += item.itemTax;
+	  	})
+	  	console.log('running total', runningTotal)
+	  	return runningTotal;
+	  }
 
+  	console.log(this.calculateRunningTotal(ctrl.billItems))
+
+
+  	this.runningTotal = this.calculateRunningTotal(this.billItems);
 
   });
